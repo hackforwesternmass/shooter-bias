a0bf639dfdd8961a8f5e6cbeb2352c8f6feef3a4
Aaron Smith <crypticsymbols@gmail.com>
2015-06-07 11:09:18 -0400

    adding csrf token to Drupal.settings var,

--- sites/all/themes/bootstrap/node--shell.tpl.php
+++ sites/all/themes/bootstrap/node--shell.tpl.php
@@ -1,3 +1,15 @@
+<?php
+
+drupal_add_js(array(
+  'myModule' => array(
+    'csrfToken' => drupal_get_token(),
+    )
+  ), 
+  'setting'
+);
+
+?>
+
 <p>Drop the JS app HTML in here, sites/all/themes/bootstrap/node--shell.tpl.php</p>
 
 <p>Add any assets to sites/all/themes/bootstrap/hackathon</p>
@@ -7,3 +19,29 @@
 <p>Otherwise all of our work will be completely destroyed if someone tries to update Drupal without us there to tell them how to save it :(</p>
 
 <p>API auth token can be loaded from GET /services/session/token and used for session auth (https://groups.drupal.org/node/358308)</p>
+
+<p>POST request to results create endpoint example:</p>
+
+<pre>
+  // Psuedocode - This needs to have the correct callback to actually set the var
+  token = jQuery.get('/services/session/token');
+
+  jQuery.ajax({
+    type: "POST",
+    url: '/api/tests/create',
+    data: {
+      "node": {
+        "type":"page",
+        "title":"Page submitted via entity JSON REST",
+        "body": {
+            "value": "<p>test</p>\n",
+            "format": "filtered_html"
+        }
+      }
+    },
+    contentType: 'application/json',
+    beforeSend: function(xhr, settings){
+      xhr.setRequestHeader("X-CSRF-Token", token);
+    }
+  });
+</pre>
