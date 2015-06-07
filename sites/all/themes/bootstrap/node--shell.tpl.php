9198ded6784b4f64564a7546967f0d4e37a1b281
Aaron Smith <crypticsymbols@gmail.com>
2015-06-06 22:52:21 -0400

    adding tpl override

---
+++ sites/all/themes/bootstrap/node--shell.tpl.php
@@ -0,0 +1,5 @@
+<p>Drop the JS app HTML in here, sites/all/themes/bootstrap/node--shell.tpl.php</p>
+
+<p>Add any assets to sites/all/themes/bootstrap/hackathon</p>
+
+<p>Change asset URLs to /sites/all/themes/bootstrap/hackathon/[asset]</p>
