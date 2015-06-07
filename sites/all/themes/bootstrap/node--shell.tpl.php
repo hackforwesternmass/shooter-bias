f07863eb73728cd1d511efc1414259dc2d0fd0f9
Aaron Smith <crypticsymbols@gmail.com>
2015-06-06 22:57:22 -0400

    sfgh

--- sites/all/themes/bootstrap/node--shell.tpl.php
+++ sites/all/themes/bootstrap/node--shell.tpl.php
@@ -3,3 +3,5 @@
 <p>Add any assets to sites/all/themes/bootstrap/hackathon</p>
 
 <p>Change asset URLs to /sites/all/themes/bootstrap/hackathon/[asset]</p>
+
+<p>Otherwise all of our work will be completely destroyed if someone tries to update Drupal without us there to tell them how to save it :(</p>
