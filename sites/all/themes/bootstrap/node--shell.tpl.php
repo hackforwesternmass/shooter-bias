33c9703220c2f730c94a435f7019cfa004783a25
Aaron Smith <crypticsymbols@gmail.com>
2015-06-06 23:13:21 -0400

    notes, EOD

--- sites/all/themes/bootstrap/node--shell.tpl.php
+++ sites/all/themes/bootstrap/node--shell.tpl.php
@@ -5,3 +5,5 @@
 <p>Change asset URLs to /sites/all/themes/bootstrap/hackathon/[asset]</p>
 
 <p>Otherwise all of our work will be completely destroyed if someone tries to update Drupal without us there to tell them how to save it :(</p>
+
+<p>API auth token can be loaded from GET /services/session/token and used for session auth (https://groups.drupal.org/node/358308)</p>
