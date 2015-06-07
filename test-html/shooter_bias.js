790c40d9b2bc1ef869b8f658f39d0c21d99ac2e2
Patrick Foley <pfoley@gmail.com>
2015-06-07 07:17:51 -0400

    comments comments comments, made more javascripty, watching keys... have but where watch keys twice

--- test-html/shooter_bias.js
+++ test-html/shooter_bias.js
@@ -1,12 +1,15 @@
 var ShooterBias = window.ShooterBias || {};
 
-// underscores. because i normally write coffeescript.
 
-ShooterBias.get_and_show_slides = function() {
-    ShooterBias.get_slides(ShooterBias.show_slides);
+ShooterBias.CURRENT_TEST = {},
+
+ShooterBias.CAN_STILL_HIT_KEY = false,
+
+ShooterBias.getAndShowSlides = function() {
+    ShooterBias.getSlides(ShooterBias.showSlides);
 },
 
-ShooterBias.get_slides = function(after) {
+ShooterBias.getSlides = function(after) {
     console.log("getting slides");
     $.ajax('/test-json//test.json').error(function() {
         alert("An error occurred when getting test json");
@@ -17,7 +20,18 @@ ShooterBias.get_slides = function(after) {
 
 },
 
-ShooterBias.show_slides = function(data) {
+// utility function to get a photo froom an array of photos based on it's url 
+ShooterBias.getPhotoFromUrl = function(photos, url){
+    for(i=0; i< photos.length; i++){
+        var photo = photos[i];
+        if(photo.image_url ==  url){
+            return photo;
+        }
+    }
+    return null;
+}, 
+
+ShooterBias.showSlides = function(data) {
 
     console.log("showing slides");
 
@@ -39,9 +53,11 @@ ShooterBias.show_slides = function(data) {
     // for each trial 
     $.each(data[0].trials, function() {
 
+        var trialPhotos = this.photos;
+
         // get an array of just the photo urls
         var just_urls = [];
-        $.each(this.photos, function() {
+        $.each(trialPhotos, function() {
             just_urls.push(this.image_url);
         });
 
@@ -55,30 +71,33 @@ ShooterBias.show_slides = function(data) {
             // called when all are loaded 
             all: function() {
 
-                // "this" is an an array of IMG tags after precaching
+                // "this" is an an array of IMG tags after preloading
                  var cached_imgs = this;
 
               
                 // this function calls itself between 600ms intervals
-                var show_next_image = function() {
+                var showNextImage = function() {
 
                     var img = cached_imgs.pop();
+                    var currentPhoto = ShooterBias.getPhotoFromUrl(trialPhotos, $(img).attr('src'));
 
+                    // if this is the last image in array,  capture keys
+                    if (cached_imgs.length == 0) {
+                        ShooterBias.CAN_STILL_HIT_KEY = true;
+                        ShooterBias.watchKeys(reactions, currentPhoto);
+                    }
 
                     // calls complete after 600ms
                     var timer = new Tock({
                         countdown: true,
                         interval: 10,
                         complete: function() {
+   
+                            ShooterBias.CAN_STILL_HIT_KEY = false;
 
-                            // if is last image capture keys
-                            if (cached_imgs.length == 1) {
-                                ShooterBias.watch_keys();
-                            }
 
                             // show next image 
-                            ShooterBias.show_slide(img);
-                            console.log(img);
+                            ShooterBias.showSlide(img);
 
 
                             // start another trial here TOOD
@@ -88,10 +107,8 @@ ShooterBias.show_slides = function(data) {
                             // show score here TOOD
 
 
-
-                            // 
                             if (cached_imgs.length > 0) {
-                                show_next_image();
+                                showNextImage();
                             }
 
                         },
@@ -104,7 +121,7 @@ ShooterBias.show_slides = function(data) {
                     timer.start(600);
                 }
 
-                show_next_image();
+                showNextImage();
             }
         });
     });
@@ -112,25 +129,66 @@ ShooterBias.show_slides = function(data) {
 },
 
 
-
-ShooterBias.show_slide = function(image) {
-
-    
+ShooterBias.showSlide = function(image) {
     $(image).addClass('center-block');   
     $('#photo_container').html(image);
-     console.log("showing " + image);
 },
 
-ShooterBias.watch_keys = function() {
+
+// here we watch the keystrokes using mousetrap 
+ShooterBias.watchKeys = function(reactions, photo) {
+
+    console.log("watchKeys");
+
+    // iterate through all of the possible reaction keys
+    for( i = 0; i < reactions.length; i++){
 
 
-    Mousetrap.bind('s', function() {
-        console.log("ima keybinding")
-    });
-    Mousetrap.bind('k', function() {
-        console.log("ima keybinding")
-    });
+        var currentReaction = reactions[i];
 
+        // iterate through the values for our reactions and bind a positive or negative outcome to the key
+        for( x=0; x< photo.reaction_values; x++){
+            
+            var reactionValue = photo.reaction_values[x];
+            if(reactionValue.reaction_id == currentReaction.id){
+                if(reactionValue.is_positive){
+                  Mousetrap.bind(currentReaction.key, ShooterBias.positiveKey);
+                }else {
+                  Mousetrap.bind(currentReaction.key, ShooterBias.negativeKey);   
+                }   
+            }
+        }
+    }
+
+}, 
+
+
+// here we will increment or decrement the score and show it
+ShooterBias.positiveKey = function() {
+
+    if(ShooterBias.CAN_STILL_HIT_KEY){
+
+        // only allow a key hit once 
+        ShooterBias.CAN_STILL_HIT_KEY = false;
+
+
+        console.log("show score!");
+
+        // TODO add to score here...
+
+    }else {
+
+
+        // TODO show too slow 
+
+        console.log("showing too slow");
+
+    }
+    console.log("positiveKey");
+}, 
+
+ShooterBias.negativeKey = function() {
+    console.log("positiveKey");
 }
 
 
@@ -142,4 +200,4 @@ ShooterBias.watch_keys = function() {
 // between screens tell if timed out
 
 
-// editable milliseconds
+// editable milliseconds TODO
